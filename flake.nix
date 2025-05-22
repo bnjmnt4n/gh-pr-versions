fc864fb63e5d6138aea85980cb3af2f05bc860d0
Benjamin Tan <benjamin@dev.ofcr.se>
2025-05-22 17:01:47 +0800

    Nix flake: Add overlay

--- flake.nix
+++ flake.nix
@@ -3,19 +3,32 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
     systems = ["aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux"];
     forEachSystem = systems: f: builtins.foldl' (acc: system: nixpkgs.lib.recursiveUpdate acc (f system)) {} systems;
   in
-    forEachSystem systems (system: let
+    {
+      overlays.default = final: prev: {
+        gh-pr-versions = self.packages.${final.system}.gh-pr-versions;
+      };
+    }
+    // (forEachSystem systems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      packageVersion = (builtins.fromJSON (builtins.readFile ./package.json)).version;
     in {
-      packages.${system}.default = pkgs.buildNpmPackage rec {
-        pname = "gh-pr-versions";
-        version = (builtins.fromJSON (builtins.readFile ./package.json)).version;
-        src = ./.;
-        npmDepsHash = "sha256-HJu39rarm9Wx1Z/2dM2DWLTNqnzFtvTwlxKQjF2CoHI=";
-        npmBuildScript = "prepublishOnly";
+      packages.${system} = {
+        gh-pr-versions = pkgs.buildNpmPackage rec {
+          pname = "gh-pr-versions";
+          version = "${packageVersion}-unstable-${self.shortRev or "dirty"}";
+          src = ./.;
+          npmDepsHash = "sha256-HJu39rarm9Wx1Z/2dM2DWLTNqnzFtvTwlxKQjF2CoHI=";
+          npmBuildScript = "prepublishOnly";
+        };
+        default = self.packages.${system}.gh-pr-versions;
       };
 
       devShells.${system}.default = pkgs.mkShell {
@@ -24,5 +37,5 @@
           prettierd
         ];
       };
-    });
+    }));
 }
